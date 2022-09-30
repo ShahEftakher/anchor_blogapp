284ea75c04da38a3c106a390fe0a3aebbefbea4a
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-30 11:03:39 +0600

    Added next app

---
+++ app/installSolanaDeps.sh
@@ -0,0 +1,1 @@
+yarn add @project-serum/anchor @solana-mobile/wallet-adapter-mobile @solana/wallet-adapter-base @solana/wallet-adapter-react @solana/wallet-adapter-react-ui @solana/wallet-adapter-wallets
